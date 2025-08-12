8184f575a05f0c2a7994eb4ea469fc3cdef263ea
Felipe Sérgio <92553373+lipesdf@users.noreply.github.com>
2025-08-12 17:49:22 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./_components/navbar/navbar.component";
+import { ButtonPrimaryComponent } from "./_components/button-primary/button-primary.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, ButtonPrimaryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
