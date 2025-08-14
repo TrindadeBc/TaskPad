598b33634ffdf818259efd153c16147a35a38986
felipe note <felipesdf2015@gmail.com>
2025-08-13 22:03:08 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,31 @@ import { ButtonPrimaryComponent } from "./_components/button-primary/button-prim
 import { TarefaComponent } from './_components/tarefa/tarefa.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormularioComponent } from "./_components/formulario/formulario.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, ButtonPrimaryComponent, TarefaComponent, FormularioComponent],
+  imports: [RouterOutlet, NavbarComponent, ButtonPrimaryComponent, TarefaComponent, FormularioComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'taskPad';
+
+  tarefas: any[] = [];
+
+  novaTarefa() {
+    this.tarefas.push({ nome: '', descricao: '' });
+  }
+
+  atualizarTarefa(tarefa: any) {
+    // Futuramente poderia salvar no backend
+  }
+
+  removerTarefa(tarefa: any) {
+    const index = this.tarefas.indexOf(tarefa);
+    if (index > -1) {
+      this.tarefas.splice(index, 1);
+    }
+  }
 }
