e6ee39aed67cd715b60e9dd3cb7dffda1ae2778b
Felipe Sérgio <92553373+lipesdf@users.noreply.github.com>
2025-08-15 09:53:12 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
-import { ButtonPrimaryComponent } from "./_components/button-primary/button-primary.component";
-import { TarefaComponent } from './_components/tarefa/tarefa.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { FormularioComponent } from "./_components/formulario/formulario.component";
 import { CommonModule } from '@angular/common';
 
